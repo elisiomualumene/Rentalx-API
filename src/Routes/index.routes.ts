6fe61d8807a7a3a705b8bb9714d54d22a53c0bba
Elisio Mualumene <elisio.mualumene@nzooji.com>
2022-09-07 22:42:03 +0100

    :sparkles: refactoring specifications router

--- src/Routes/index.routes.ts
+++ src/Routes/index.routes.ts
@@ -4,7 +4,7 @@ const IndexRoutes = Router();
 
 
 IndexRoutes.get("/", (request, response) => {
-    return response.json({"message": "servidor rodando na porta 3030", "created by": "Elisio Mualumene", "Company": "Nzooji"})
+    return response.json({"message": "servidor rodando na porta 3030", "created by": "Elisio Mualumene", "Company": "unknown"})
 })
 
 export { IndexRoutes };
--- src/Routes/specifcations.routes.ts
+++ src/Routes/specifcations.routes.ts
@@ -1,24 +1,11 @@
 import { Router } from "express";
-import { SpecificationRepository } from "../modules/cars/repositories/Specification/SpecificationRepository";
-import { CreateSpecificationService } from "../modules/cars/services/CreateSpecificationService";
+import { CreateSpecificationController } from "../modules/cars/useCases/createSpecifications";
+import { ListSpecificationsController } from "../modules/cars/useCases/listSpecifications";
 
 const specificationRoutes = Router();
-const specificationRepository = new SpecificationRepository();
 
-specificationRoutes.post('/', (request, response) => {
-    const {name, description} = request.body;
+specificationRoutes.post('/', (request, response) => {return CreateSpecificationController.handle(request, response)});
 
-    const createSpecificationService = new CreateSpecificationService(specificationRepository)
-
-    createSpecificationService.execute({name, description});
-
-    return response.status(201).json({'message': 'Specification was added'})
-});
-
-specificationRoutes.get('/', (request, response) => {
-    const allSpecifications = specificationRepository.list();
-
-    return response.status(200).json(allSpecifications)
-});
+specificationRoutes.get('/', (request, response) => {return ListSpecificationsController.handle(request, response) });
 
 export {specificationRoutes};
