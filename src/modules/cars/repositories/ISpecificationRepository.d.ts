d326c94b6830d9d3592a8af515922fe3ba16429b
Elisio Mualumene <elisio.mualumene@nzooji.com>
2022-09-07 21:38:41 +0100

    :octocat: refactoring repositories and

--- src/modules/cars/repositories/ISpecificationRepository.d.ts
+++
@@ -1,9 +0,0 @@
-import { Specification } from "../model/Specification";
-
-interface ISpecificationRepository{
-    create({name, description}: ISpecificationDTO): void;
-    findByName(name: string): Specification;
-    list(): Specification[]
-}
-
-export {ISpecificationRepository}
