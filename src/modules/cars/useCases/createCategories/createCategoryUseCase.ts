df34907deb4c2b900393b21fec1ce2739e506453
Elisio Mualumene <elisio.mualumene@nzooji.com>
2022-09-07 18:45:54 +0100

    :zap: create category controller and usecase

---
+++ src/modules/cars/useCases/createCategories/createCategoryUseCase.ts
@@ -0,0 +1,18 @@
+import { ICategoryRepository } from "../../repositories/ICategoryRepository";
+
+class CreateCategoryUseCase{
+
+    constructor(private repositoryCategories: ICategoryRepository){}
+
+    execute({ name, description }: IRequest): void{
+        const cateogryAlreadyExists = this.repositoryCategories.findByName(name);
+
+        if(cateogryAlreadyExists) {
+            throw new Error("Category Already exists!");
+        }
+
+        this.repositoryCategories.create({name, description});
+    };
+};
+
+export { CreateCategoryUseCase };
