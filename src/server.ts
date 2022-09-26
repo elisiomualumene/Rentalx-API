972d68480e952aaf3117f95f8ce921ddb05cf080
Elisio Mualumene <elisio.mualumene@nzooji.com>
2022-09-26 16:26:57 +0100

    :zap: server updated

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,23 @@
+import "reflect-metadata"
+
 import express, { json } from "express"
 import dotenv from "dotenv"
 import { router } from "./Routes";
 import swaggerUI from "swagger-ui-express"
-
 import swaggerFile from "./swagger.json"
+
+
 
 const app = express();
 dotenv.config();
 app.use(json());
 
-const port = process.env.PORT_SERVER;
+const port = process.env.PORT_SERVER || 3030;
 
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerFile))
 
 app.use(router)
 
 app.listen(port, () => {
-    console.log(`Server is Running on port ${process.env.PORT_SERVER}`);
+    console.log(`Server is Running on port ${port}`);
 });
