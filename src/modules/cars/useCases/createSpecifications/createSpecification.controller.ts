db3797c170391962ae17f82ad760540c365ca305
Elisio Mualumene <elisio.mualumene@nzooji.com>
2022-09-26 16:27:35 +0100

    :hammer: create specificstions

--- src/modules/cars/useCases/createSpecifications/createSpecification.controller.ts
+++ src/modules/cars/useCases/createSpecifications/createSpecification.controller.ts
@@ -7,7 +7,7 @@ class createSpecificationController {
     handle(request: Request, response: Response): Response{
     
     const {name, description} = request.body;
-
+    
     this.CreateSpecificationUseCase.execute({name, description});
 
     return response.status(201).json({'message': 'Specification was added'})
