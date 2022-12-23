348380e17b3aaa71be956375ea3c1ae440d31853
Elisio Mualumene <elisiomualumene@gmail.com>
2022-12-23 23:27:27 +0100

    :memo: interfaces for usecases and repositories

--- src/modules/cars/types/useCases.d.ts
+++ src/modules/cars/types/useCases.d.ts
@@ -9,3 +9,14 @@ export interface IImportCategory{
     name: string,
     description: string;
 }
+
+export interface ICreateCarProps {
+    name: string;
+    description: string;
+    daily_rate: number;
+    available: boolean;
+    licence_plate: string;
+    fine_amount: number;
+    brand: string;
+    category_id: string;
+}
