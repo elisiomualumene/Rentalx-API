156a9962d935d93b06c89beba652e26301a9c2fb
Elisio Mualumene <elisiomualumene@gmail.com>
2022-12-23 23:28:37 +0100

    :zap: dependecy injection and database updates

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -10,6 +10,9 @@ import { SpecificationRepository } from "../../modules/cars/infraestructure/type
 import { IUserRepository } from "../../modules/accounts/repositories/User/IUserRepository"
 import { UserRepository } from "../../modules/accounts/repositories/User/implementation/UserRepository"
 
+import { ICarRepository } from "@modules/cars/repositories/Car/ICarRepository"
+import { CarRepository } from "../../modules/cars/infraestructure/typeorm/repositories/Cars/CarRepositoryImp"
+
 // interface categoriesRepository
 container.registerSingleton<ICategoryRepository>(
     "CategoryRepository",
@@ -26,4 +29,9 @@ container.registerSingleton<ISpecificationRepository>(
 container.registerSingleton<IUserRepository>(
     "UserRepository",
     UserRepository
+)
+
+container.registerSingleton<ICarRepository>(
+    "CarRepository",
+    CarRepository
 )
--- src/shared/infraestructure/database/index.ts
+++ src/shared/infraestructure/database/index.ts
@@ -2,8 +2,17 @@ import {Category} from "../../../modules/cars/infraestructure/typeorm/entities/C
 import { Specification } from "../../../modules/cars/infraestructure/typeorm/entities/Specification";
 import { User } from "../../../modules/accounts/infraestructure/typeorm/entities/User";
 
+import {CreateCategory1664470153748 as CategoryMigration} from "./migrations/1664470153748-CreateCategory"
+import { createSpecification1667240325686 as SpecificationMigration } from "./migrations/1667240325686-CreateSpecifications";
+import { CreateUsers1668178211212 as UserMigration } from "./migrations/1668178211212-CreateUsers";
+import { AlterUserAddAvatar1668599236908 as AvatarMigration } from "./migrations/1668599236908-AlterUserAddAvatar";
+import { CreateCars1671819999593 as CarsMigration } from "./migrations/1671819999593-CreateCars";
+
 import { DataSource } from 'typeorm';
 
+const Entity = [Category, Specification, User];
+
+const Migrations = [CategoryMigration, SpecificationMigration, UserMigration, AvatarMigration, CarsMigration]
 
 export const AppDataSource = new DataSource({
     type: 'postgres',
@@ -12,8 +21,8 @@ export const AppDataSource = new DataSource({
     username: 'rentalx',
     password: 'test',
     database: 'rentalx',
-    entities: [Category, Specification, User],
+    entities: Entity,
     synchronize: true,
     logging: true,
-    migrations: ["./src/database/migrations/*.ts"],
+    migrations: Migrations,
   });
