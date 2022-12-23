21343e8ba22edeb35635ce5d30a9627da60ecff7
Elisio Mualumene <elisiomualumene@gmail.com>
2022-12-23 23:24:20 +0100

    :sparkles: server changes

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import {app} from "./shared/infraestructure/http/app"
 
 const port = process.env.PORT_SERVER || 3030;
 
+
 app.listen(port, () => {
     console.log(`Server is Running on port ${port}`);
 });
