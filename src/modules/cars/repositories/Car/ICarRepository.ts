b7df24c71bcfcc8b178e85da8f35a05f36f3d064
Elisio Mualumene <elisiomualumene@gmail.com>
2022-12-27 13:00:34 +0100

    :memo: abstract car repository updated

--- src/modules/cars/repositories/Car/ICarRepository.ts
+++ src/modules/cars/repositories/Car/ICarRepository.ts
@@ -3,7 +3,9 @@ import {ICreateCarDTO} from '../../types/dtos';
 
 interface ICarRepository {
     create(data: ICreateCarDTO): Promise<Car>
-    list(): Promise<Car[]>
+    list(): Promise<Car[]>;
+    findById(id: string): Promise<Car>
+    listByAvailable(brand?: string, category_id?: string, name?: string): Promise<Car[]>;
     findByLicencePlate(licence_plate: string): Promise<Car>
 }
 
