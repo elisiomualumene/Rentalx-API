7a071cd54b727ed466ba2c5e7c31d3d508ca6a3e
Elisio Mualumene <elisiomualumene@gmail.com>
2022-12-27 13:02:40 +0100

    :memo: cars routes

--- src/shared/infraestructure/http/Routes/cars.routes.ts
+++ src/shared/infraestructure/http/Routes/cars.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { CreateCarController } from '../../../../modules/cars/useCases/createCar/CreateCarController';
 import { ListCarsController } from '../../../../modules/cars/useCases/listCars/listCarsController';
+import { ListByAvailableController } from '../../../../modules/cars/useCases/listByAvailable/ListByAvailableController';
 import { EnsureAdmin } from '../middlewares/ensureAdmin';
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 
@@ -8,9 +9,11 @@ const carRoutes = Router();
 
 const createCarController = new CreateCarController();
 const listCarsController = new ListCarsController();
+const listByAvailableController = new ListByAvailableController()
 
 carRoutes.post('/',ensureAuthenticated, EnsureAdmin, createCarController.handle);
 carRoutes.get('/',ensureAuthenticated, EnsureAdmin, listCarsController.handle);
+carRoutes.get('/available', ensureAuthenticated, EnsureAdmin, listByAvailableController.handle)
 
 
 export { carRoutes };
