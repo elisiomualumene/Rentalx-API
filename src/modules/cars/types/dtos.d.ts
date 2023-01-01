e41f1f21c8c16506e60a79737261355cba258203
Elisio Mualumene <elisiomualumene@gmail.com>
2023-01-01 16:46:29 +0100

    dtos

--- src/modules/cars/types/dtos.d.ts
+++ src/modules/cars/types/dtos.d.ts
@@ -1,3 +1,4 @@
+import { Specification } from '../../../modules/cars/infraestructure/typeorm/entities/Specification';
 // DTO = Data Transfer Object
 
 export interface ICreateCategoryDTO {
@@ -18,4 +19,6 @@ export interface ICreateCarDTO {
     fine_amount: number;
     brand: string;
     category_id: string;
+    specifications?: Specification[];
+    id?: string;
 }
