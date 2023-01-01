c2214115df62eaaf838a7f2c36d85d0ca4d10c6b
Elisio Mualumene <elisiomualumene@gmail.com>
2023-01-01 16:46:58 +0100

    car specification use case

--- src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.ts
+++ src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.ts
@@ -2,24 +2,37 @@ import 'reflect-metadata';
 import { AppError } from './../../../../shared/errors/AppError';
 import { injectable, inject } from "tsyringe";
 import { ICarRepository } from '../../../../modules/cars/repositories/Car/ICarRepository';
+import { ISpecificationRepository } from '../../../../modules/cars/repositories/Specification/implementation/ISpecificationRepository';
+import { Car } from '../../../../modules/cars/infraestructure/typeorm/entities/Car';
 
 
 interface IRequest {
-    car_id: string;
-    specification_id: string[]
+    car_id: any;
+    specification_id: any[]
 }
 
 @injectable()
 export class CreateCarSpecificationUseCase{
     constructor(
         @inject('CarRepository')
-        private carRepository: ICarRepository
+        private carRepository: ICarRepository,
+        @inject('RepositorySpecification')
+        private specificationRepository: ISpecificationRepository
     ){}
-    async execute({car_id,specification_id}:IRequest): Promise<void>{
+    async execute({car_id,specification_id}:IRequest): Promise<Car>{
         const carExists = await this.carRepository.findById(car_id)
 
         if(!carExists){
             throw new AppError('Car already Exists')
         }
+        const specifications = await this.specificationRepository.findByIds(specification_id)
+
+        carExists.specifications = specifications;
+
+        await this.carRepository.create(carExists);
+
+        console.log(carExists);
+        
+        return carExists
     }
 }
