75da5834ccf8c584a734a5c560f3aad275c1c91e
Elisio Mualumene <elisiomualumene@gmail.com>
2023-01-04 21:31:12 +0100

    :zap:

--- src/modules/rentals/infraestructure/typeorm/repositories/IRentalRepository.ts
+++
@@ -1,7 +0,0 @@
-import { Rental } from "../entities/Rental";
-
-export interface IRentalRepository {
-    create(data: ICreateRentalDTO): Promise<Rental>
-    findOpenRentalByCar(car_id: string): Promise<Rental>;
-    findOpenRentalByUser(user_id: string): Promise<Rental>;
-}
