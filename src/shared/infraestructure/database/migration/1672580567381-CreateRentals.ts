10774c1f6f57ceddd7090c6c40ece1750049493d
Elisio Mualumene <elisiomualumene@gmail.com>
2023-01-04 23:20:53 +0100

    :zap:

--- src/shared/infraestructure/database/migration/1672580567381-CreateRentals.ts
+++ src/shared/infraestructure/database/migration/1672580567381-CreateRentals.ts
@@ -5,15 +5,15 @@ export class CreateRentals1672580567381 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.createTable(
             new Table({
-                name: 'rentals',
+                name: 'Rental',
                 columns: [
                     {name: 'id', type: 'uuid', isPrimary: true},
                     {name: 'car_id', type: 'uuid'},
                     {name: 'user_id', type: 'uuid'},
                     {name: 'star_date', type: 'timestamp', default: 'now()'},
-                    {name: 'end_date', type: 'timestamp'},
+                    {name: 'end_date', type: 'timestamp', isNullable: true},
                     {name: 'expected_return_date', type: 'timestamp'},
-                    {name: 'total', type: 'numeric'},
+                    {name: 'total', type: 'numeric', isNullable: true},
                     {name: 'created_at', type: 'timestamp', default: 'now()'},
                     {name: 'updated_at', type: 'timestamp', default: 'now()'}
                 ],
