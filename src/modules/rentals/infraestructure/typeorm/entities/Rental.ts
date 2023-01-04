4fe4d2dd4cccfeddf89731d6082567ad06e8f5fa
Elisio Mualumene <elisiomualumene@gmail.com>
2023-01-04 21:31:06 +0100

    :zap:

--- src/modules/rentals/infraestructure/typeorm/entities/Rental.ts
+++ src/modules/rentals/infraestructure/typeorm/entities/Rental.ts
@@ -1,20 +1,39 @@
 import { v4 as UUIDV4 } from "uuid";
+import { PrimaryColumn, Column, CreateDateColumn, Entity, UpdateDateColumn } from "typeorm";
 
-
+@Entity('Rental')
 export class Rental {
+    @PrimaryColumn()
     id?: string;
+
+    @Column()
     car_id!: string;
+
+    @Column()
     user_id!: string;
+
+    @Column({default: 'now()'})
     start_date!: Date;
+
+    @Column({nullable: true})
     end_date!: Date;
+
+    @Column()
     expected_return_date!: Date;
+
+    @Column({nullable: true})
     total!: number;
+    
+    @CreateDateColumn()
     created_at!: Date;
+
+    @UpdateDateColumn()
     updated_at!: Date;
 
     constructor(){
         if(!this.id){
             this.id = UUIDV4();
+            this.start_date = new Date()
         }
     }
 }
